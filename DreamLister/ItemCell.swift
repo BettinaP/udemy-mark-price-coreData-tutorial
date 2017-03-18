d20c45ee0030f5eacfc1ea50558e8e36f4b6f973
BettinaP <bettinaprophete@gmail.com>
2017-03-17 23:14:22 -0400

    connected outlets and added tvc methods

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,22 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Bettina on 3/17/17.
+//  Copyright © 2017 Bettina Prophete. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnail: UIImageView!
+    
+    @IBOutlet weak var title: UILabel!
+
+    @IBOutlet weak var price: UILabel!
+
+    @IBOutlet weak var details: UILabel!
+
+
+}
--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +28,18 @@ class MainVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
 }
 
