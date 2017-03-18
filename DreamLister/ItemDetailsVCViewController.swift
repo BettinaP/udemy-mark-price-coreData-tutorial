650a3d96dd11066687020104347188fbbb5bdf7e
BettinaP <bettinaprophete@gmail.com>
2017-03-18 12:48:06 -0400

    set picker to fetched stores

--- DreamLister/ItemDetailsVCViewController.swift
+++ DreamLister/ItemDetailsVCViewController.swift
@@ -7,32 +7,80 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailsVCViewController: UIViewController {
-
+class ItemDetailsVCViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var detailsField: UITextField!
+    
+    var stores = [Store]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let topItem = self.navigationController?.navigationBar.topItem{
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         }
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
+        generateTestData()
+        getStores()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func generateTestData() {
+        let store = Store(context: context)
+        store.name = "Best Buy"
+        
+        let store2 = Store(context: context)
+        store2.name = "Office Depot"
+        
+        let store3 = Store(context: context)
+        store3.name = "Walmart"
+        
+        let store4 = Store(context: context)
+        store4.name = "Tesla Dealership"
+        
+        let store5 = Store(context: context)
+        store5.name = "Amazon"
+        
+        let store6 = Store(context: context)
+        store6.name = "Target"
+        
+        ad.saveContext()
     }
-    */
-
+    
+    func getStores() {
+        let storeFetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do{
+            self.stores = try context.fetch(storeFetchRequest)
+            self.storePicker.reloadAllComponents()
+        } catch {
+            //handle error
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //update when selected
+    }
 }
